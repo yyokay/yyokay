1fb09cf7e06b4d62af964e01ca895161a7fb1174
yyokay <ssydxa219@125.com>
2019-02-27 16:14:21 +0800

    init

--- yyok-shares/yyok-share-oauths/yyok-share-oauth-server/src/main/java/org/yyok/oauth/controller/LoginController.java
+++ yyok-shares/yyok-share-oauths/yyok-share-oauth-server/src/main/java/org/yyok/oauth/controller/LoginController.java
@@ -2,19 +2,50 @@ package org.yyok.oauth.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @RestController
 public class LoginController {
 
-    @RequestMapping(value = "/login")
+    /*@RequestMapping(value = "/login")
     public String login() {
 
         return "login";
-    }
+    }*/
     @RequestMapping(value = "/home")
     public String home() {
         System.err.println("123");
         return "home";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody Principal user(Principal user){
+        return user;
+    }
+
+    /**
+     * @see org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter
+     * @param model
+     * @param request
+     * @param error
+     * @return
+     */
+    @GetMapping("/login")
+    public String login(Model model, HttpServletRequest request, @RequestParam(value = "error", required = false) String error) {
+        CsrfToken token = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (token != null) {
+            model.addAttribute("tokenName",token.getParameterName());
+            model.addAttribute("token",token.getToken());
+        }
+        if (error != null) {
+            model.addAttribute("error", "用户名或密码错误");
+        }
+        return "login";
     }
 
 }
